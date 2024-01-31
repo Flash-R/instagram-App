84761be97707424cd06192c2ddb6ecf0809c6957
Tumwesigye Ronnie <tumronald1@gmail.com>
2024-01-31 22:50:53 +0300

    handles API call, validations and API status messages

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -1,12 +1,17 @@
 import React,{useState} from "react";
+import axios from "axios";
 
 
 
 
 const Signup = ()=>{
 
-    const  [user , setUser] = useState({name:"",email:"",password:"",confirmPassword});
+    const  [user , setUser] = useState({name:"",email:"",password:"",confirmPassword:""});
     const  { name, email, password, confirmPassword }= user;
+
+    // useState variables to handle the succes and error messages
+    const [successMessage, setSuccessMessage]=useState("");
+    const [errorMessage,setErrorMessage]=useState("");
 
 
     function updateUser(e){
@@ -15,30 +20,60 @@ const Signup = ()=>{
         setUser({...user, [key]: value})
     }
 
+    async function handleSignup(e){
+        e.preventDefault();
 
+        // validations
+        if (!name || !email || !password || !confirmPassword){
+            alert("All fields are required");
+        }else if (password !== confirmPassword) {
+            alert("Passwords do not match");
+            return;
+        }
+        try {
+            const response = await axios.post("https://instagram-express-app.vercel.app/api/auth/signup",
+            {
+                name: name,
+                "email": email,
+                "password": password,
+            });
+            console.log("success",response.data.message);
+            setSuccessMessage(response.data.message);
+            setErrorMessage("")
+            
+        } catch (error) {
+            console.log("failure",error.response.data.message);
+            setErrorMessage(error.response.data.message);
+            setSuccessMessage("")
+        }
+    }
 
     return (
         <div className="container"> 
+            {successMessage  && <h5 className="sucess">{successMessage}</h5>}
+            {errorMessage && <h5 className='danger'>{errorMessage}</h5>}
             <h1>Sign Up</h1>
-            <form onSubmit={handleSubmit}>
-                <input type='text' name='name' placeholder='Name'
+            <form onSubmit={handleSignup}>
+                <input type='text' name='name' placeholder='Name...'
                     onChange={updateUser}
                     value={name}
                 />
-                <input type='text' name='email' placeholder='Email'
+                <input type='text' name='email' placeholder='Email...'
                     onChange={updateUser}
                     value={email}
                 />
-                <input type='text' name='password' placeholder='Password'
+                <input type='password' name='password' placeholder='Password'
                     onChange={updateUser}
                     value={password}
                 />
-                <input type='text' name='confirmPassword' placeholder='Confirm Password'
+                <input type='password' name='confirmPassword' placeholder='Confirm Password'
                     onChange={updateUser}
-                    value={confirmpassword}
+                    value={confirmPassword}
                 />
-
+                <button type="submit">Signup</button>
             </form>
         </div>
     )
 }
+
+export default Signup;
